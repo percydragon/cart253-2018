24ac96419c06a624103af9e213aefe82760a9168
Percy Dragon <percy.s.dragon@gmail.com>
2018-10-30 16:32:30 -0400

    ex6: added a + to ball.js for this.x += this.vx because the ball was just moving horizontally to eternity and not moving vertically

--- exercises/exercise 6/js/Ball.js
+++ exercises/exercise 6/js/Ball.js
@@ -26,7 +26,7 @@ function Ball(x,y,vx,vy,size,speed) {
 //NOTE the function was written updated instead of update
 Ball.prototype.update = function () {
   // Update position with velocity
-  this.x = this.vx;
+  this.x += this.vx;
   this.y += this.vy;
 
   // Constrain y position to be on screen
