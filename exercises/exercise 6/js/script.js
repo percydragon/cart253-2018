3e5b256f3227dd3fb7227769e81dad1820a0d3ad
Percy Dragon <percy.s.dragon@gmail.com>
2018-10-30 15:47:58 -0400

    ex6: added an l to var ball in script.js bc because it was missing an l

--- exercises/exercise 6/js/script.js
+++ exercises/exercise 6/js/script.js
@@ -10,7 +10,8 @@
 // Written with JavaScript OOP.
 
 // Variable to contain the objects representing our ball and paddles
-var bal;
+//NOTE var ball is written incorrectly and missing an l
+var ball;
 var leftPaddle;
 var rightPaddle;
 
