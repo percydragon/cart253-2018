3a4bc1ac8172d247522b9ea0255ea2a0e51e087b
Percy Dragon <percy.s.dragon@gmail.com>
2018-11-13 15:40:03 -0500

    ex7: added in a universe function to set up the universe, and a mouse clicked function for a true/false variable

--- exercises/exercise7/js/script.js
+++ exercises/exercise7/js/script.js
@@ -5,6 +5,7 @@
 // create a solar system
 
 //varibals
+var addPlanet = false;
 
 //setup()
 //setting up the universe
@@ -14,5 +15,21 @@ function setup() {
 
 //draw()
 function draw() {
- }
+  background(0);
+  universe();
+}
+
+//universe()
+//creating code for the universe
+
+function universe() {
+  sphere(100);
+  noStroke();
+  fill(255,210,99);
+}
+
+//mouseclicked()
+//each time the player clicks the screen, a new random planet is added
+function mouseClicked() {
+  addPlanet = true;
 }
