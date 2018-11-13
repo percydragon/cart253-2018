fd1c5917a74fff38b835c26852019c95e62d3855
Percy Dragon <percy.s.dragon@gmail.com>
2018-11-13 15:26:38 -0500

    EX7: started exercise 7

---
+++ exercises/exercise7/js/script.js
@@ -0,0 +1,18 @@
+// Percy Dragon
+// Interactive Solar System
+// A javascript 3D project
+//
+// create a solar system
+
+//varibals
+
+//setup()
+//setting up the universe
+function setup() {
+  createCanvas(700,700, WEBGL);
+}
+
+//draw()
+function draw() {
+ }
+}
