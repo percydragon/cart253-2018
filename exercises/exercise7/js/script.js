34965140f2c1d8511421cf600049ea2ec25ea1e7
Percy Dragon <percy.s.dragon@gmail.com>
2018-11-13 21:49:52 -0500

    EX7: mapped the rotation of the planets to mouseX, as long as the mouse moves, the planets move at different speeds
    I also added in more comments to further explain what I was doing. I commented out some things that were no longer valuable to the code (addPlanet and angleR += 0.005). Hopefully everything is clear and clean. Hopefully.

--- exercises/exercise7/js/script.js
+++ exercises/exercise7/js/script.js
@@ -12,12 +12,13 @@
 // in this case they quite literally do.
 
 //varibals
-var addPlanet = false;
+//var addPlanet = false;
 var angleY = 0.0;
 var angleX = 0.0;
 
 var numPlanets = 5;
 
+//array for planets because who needs to put them all in manually am i right?
 var planets = [];
 var planetsTotal = 0;
 
@@ -31,6 +32,8 @@ function setup() {
   createCanvas(1500,800, WEBGL);
 
   //adding while loop that pushes planets into planet array
+  //because im effecient (read: lazy) and no one should have to manually code in
+  //every single planet, when the code can do it for you!
   while ( planetsTotal < numPlanets) {
     planets.push(new Planet((planetsTotal*80) + 150,0,50,random(10,50)));
     planetsTotal++;
@@ -38,7 +41,7 @@ function setup() {
 
   //adding in oscillating sound
   osc = new p5.Oscillator();
-  osc.setType("cosine");
+  osc.setType("sine");
   osc.freq(random(250));
   osc.amp(1);
   osc.start();
@@ -57,17 +60,30 @@ function draw() {
 //when the other planets should be shown
 
 function universe() {
+  //implementing the sun
   background(0);
   // push();
   rotateY(angleY);
   noStroke();
+  //orangeish colour
   fill(255,210,99);
   sphere(100);
 
+  // implementing the rest of the planets
+  //they look sort of trippy and will 100% cause epilepsy
+  //when you press the mouse, the planets appear
+  //when you releat the mouse, poof! they vanish
   if (mouseIsPressed) {
+    // draws the planets in until there are no more planets to draw in
     for (var i = 0; i < planets.length; i++) {
-      rotateY(angleY);
+      var angleR = 0.0;
+      //angleR += 0.005;
+      //having the angle of rotation be connected to the mouse
+      angleR = map(mouseX,0,width,30,0);
+      rotateY(angleR);
       planets[i].display();
+      //while the mouse is pressed, there is an oscillating sound connected to both the x and y of the mouse
+      // it sounds really trippy
       osc.freq(constrain(map(mouseY,0,height,1000,0),0,1000));
       osc.amp(constrain(map(mouseX,0,width,100,1),1,100));
     }
