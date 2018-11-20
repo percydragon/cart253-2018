26c866bf73961ab934cdd704e51acefda0054177
Percy Dragon <percy.s.dragon@gmail.com>
2018-11-20 14:35:43 -0500

    ex8: started prototype

---
+++ exercises/exercise8/js/script.js
@@ -0,0 +1,23 @@
+// Percy Dragon
+// Prototype 2
+// A project about
+// pushing stars into a galaxy
+// when you click on a button
+// and the stars are connected
+// to the position of the mouse
+
+//variables
+
+var babyStars = [];
+
+//setup()
+//let's set up the galaxy
+function setup() {
+  createCanvas(1000,800);
+}
+
+//draw()
+//let's make it do shit
+function draw() {
+  background(0);
+}
